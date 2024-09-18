eb885de790bbb247d13961e9aa9006bfe09e493c
Your Name <you@example.com>
2024-09-18 15:51:56 -0500

    Reestructuracion

---
+++ js/script.js
@@ -0,0 +1,31 @@
+    
+// Cargar el contenido del Navbar con DOM
+fetch('../componentes/Navbar/navbar.html')
+    .then(response => response.text())
+    .then(data => {
+    document.getElementById('Navbar').innerHTML = data;
+
+    //Enlace para el archivo CSS del Navbar
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = '../componentes/Navbar/navbar.css';
+    document.head.appendChild(link);
+    })
+.catch(error => console.error('Error al cargar el Navbar:', error));
+
+
+    // Cargar el contenido del footer con DOM
+fetch('../componentes/Footer/footer.html')
+    .then(response => response.text())
+    .then(data => {
+    document.getElementById('footer').innerHTML = data;
+
+    // Enlace para el archivo CSS del footer
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = '../componentes/Footer/footer.css';
+    document.head.appendChild(link);
+    })
+.catch(error => console.error('Error al cargar el footer:', error));
+
+

9a2cd9a6ebfe7b8420da9ad163753a01e3567534
Camii23 <camiij.cjn@gmail.com>
2024-10-01 21:07:22 -0500

    actualizaciones dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,4 +1,4 @@
-document.getElementById('cancel-button').addEventListener('click', async () => {
+document.getElementById("cancel-button").addEventListener("click", async () => {
     const result = await Swal.fire({
         title: "¿Estás seguro que quieres cancelar el proceso?",
         icon: "warning",
@@ -16,4 +16,12 @@ document.getElementById('cancel-button').addEventListener('click', async () => {
     }else{
         Swal.fire("Continua con el proceso.");
     }
+});
+
+document.getElementById("add-button").addEventListener("click", async () => {
+    Swal.fire({
+        title: "Excelente!",
+        text: "Has agregado un nuevo producto!",
+        icon: "success"
+      });
 });
